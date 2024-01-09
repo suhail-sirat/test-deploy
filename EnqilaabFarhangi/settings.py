3699f38dbb21c839c453bc93cedcd96fbbb3f867
suhail-sirat <suhailsammim1@gmail.com>
2024-01-09 23:25:23 +0430

    Here You go

--- EnqilaabFarhangi/settings.py
+++ EnqilaabFarhangi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7m6r5r9!*a&t*!z)bxs+$2v38fk^-wf^hh3*omx@xxm3%g0nhi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
